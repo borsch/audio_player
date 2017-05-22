0527b1e5be96f864ecc6ff608b62d981483c353f
borsch <oleh.kurpiak@gmail.com>
2017-05-22 15:58:54 +0300

    Remove jquery dependency

--- app.js
+++ app.js
@@ -1,42 +1,64 @@
-$(document).ready(function () {
-    var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-    var audioElement = document.getElementById('audioElement');
-    var audioSrc = audioCtx.createMediaElementSource(audioElement);
-    var analyser = audioCtx.createAnalyser();
+var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+var audioElement = document.getElementById('audioElement');
+var audioSrc = audioCtx.createMediaElementSource(audioElement);
+var analyser = audioCtx.createAnalyser();
 
-    // Bind our analyser to the media element source.
-    audioSrc.connect(analyser);
-    audioSrc.connect(audioCtx.destination);
+// Bind our analyser to the media element source.
+audioSrc.connect(analyser);
+audioSrc.connect(audioCtx.destination);
 
-    var frequencyData = new Uint8Array(200);
+var visualizer = document.getElementById('visualizer'),
+    rects_number = 200,
+    rect_width = 5,
+    svg_height = visualizer.getAttribute('height'),
+    frequencyData = new Uint8Array(rects_number)
+    rects = [];
 
-    var visualizer = document.getElementById('visualizer');
-    var rects = visualizer.getElementsByTagName('rect');
-	rects = Array.prototype.slice.call(rects);	
-    
-    var height = visualizer.getAttribute('height');
-    height = parseInt(height);
+svg_height = parseInt(svg_height);  
 
-    var run = true;
+for (var i = 0; i < rects_number; ++i) {
+    var rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+    rect.setAttribute('y', svg_height);
+    rect.setAttribute('width', rect_width);
+    rect.setAttribute('height', 0);
+    rect.setAttribute('x', rect_width * i);
 
-    function renderChart() {
-    	if (run) {
-     	   requestAnimationFrame(renderChart);
-    	}
+    rects.push(rect);
+    visualizer.appendChild(rect);
+}
 
-        analyser.getByteFrequencyData(frequencyData);
 
-        rects.forEach(function(rect, i){
-        	rect.setAttribute('y', height/2 - frequencyData[i]/2);
-        	rect.setAttribute('height', frequencyData[i]);
-            rect.setAttribute('fill', 'rgb('+i+', '+frequencyData[i]+', '+i+')');
-        });
+var run = true;
+
+function renderChart() {
+    if (run) {
+        requestAnimationFrame(renderChart);
     }
 
-    audioElement.onended = function(e) {
-    	console.log(e);
-    	console.log('end');
-    }
+    analyser.getByteFrequencyData(frequencyData);
 
-    renderChart();
-});
+    rects.forEach(function(rect, i){
+        rect.setAttribute('y', svg_height/2 - frequencyData[i]/2);
+        rect.setAttribute('height', frequencyData[i]);
+        rect.setAttribute('fill', 'rgb('+i+', '+frequencyData[i]+', '+i+')');
+    });
+}
+
+renderChart();
+
+
+function play() {
+    document.getElementById('audioElement').play();
+}
+
+function pause() {
+    document.getElementById('audioElement').pause();
+}
+
+function increment() {
+    document.getElementById('audioElement').volume+=0.1;
+}
+
+function decrement() {
+    document.getElementById('audioElement').volume-=0.1
+}
